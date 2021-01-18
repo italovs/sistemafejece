e7bec14145a5584c1c8793dfac3abd03d523de07
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-01-17 23:43:11 -0300

    Helper de logout

--- app/controllers/administrative_controller.rb
+++ app/controllers/administrative_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class AdministrativeController < ApplicationController
+  include ApplicationHelper
   def index
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def logout_path
+    if member_signed_in?
+      destroy_member_session_path
+    elsif admin_signed_in?
+      destroy_admin_session_path
+    end
+  end
 end
