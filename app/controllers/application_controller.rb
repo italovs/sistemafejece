6816a97c583f3d5fe1e2138e8bd9948f9f29e450
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-01-19 19:17:25 -0300

    Método para redirecionar não admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  def verifica_admin
+    if !admin_signed_in?
+      if member_signed_in?
+        redirect_to member_root_path
+      else
+        redirect_to not_logged_member_root_path
+      end
+    end
+  end
 end
