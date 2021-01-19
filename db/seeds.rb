62c20dc994331e29fac74c2bbf3a6772f8a6246b
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-01-18 23:34:03 -0300

    Seeds de admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,9 @@
 
 Admin.create(email: 'admin@gti.com', password: 'voagti')
 Member.create(email: 'member@gti.com', password: '123123')
+
+
+JuniorEnterprise.create(name: 'GTi', description: 'Ases, mestres, guerreiros e exploradores do espaço')
+9.times do |i|
+  JuniorEnterprise.create(name: "EJ #{i}", description: "A #{i+2}ª melhor EJ")
+end
