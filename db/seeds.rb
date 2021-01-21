1753370dff1463f6d197c4b221a862c3d0dd7018
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-01-20 21:22:23 -0300

    Seeds 'melhoradas'

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "##### SEEDS #####"
+
+acoes = [
+  "Semeando", "Plantando", "Colhendo", "Programando", "Codando", "Fazendo deploy de", 
+  "Debuggando", "Em reunião com", "Indo buscar", "Pensando em", "Fazendo cosplay de",
+  "Fritando", "Dançando", "Jogando RPG", "Pilotando a nave com", "Estudando sobre",
+  "Vacinando", "Virando jacaré com", "No Dota com"
+]
+
 
 Admin.create(email: 'admin@gti.com', password: 'voagti')
 
+puts "#{acoes.sample} EJs..."
 JuniorEnterprise.create(name: 'GTi', description: 'Ases, mestres, guerreiros e exploradores do espaço')
 9.times do |i|
   JuniorEnterprise.create(name: "EJ #{i}", description: "A #{i+2}ª melhor EJ")
 end
 
-Member.create(email: 'member@gti.com', password: '123123', junior_enterprise_id: 1, validated: true)
 
-#3 membros comuns
-3.times do |i|
 
-end
-
-ejs = Set.new(1..10)
+puts "#{acoes.sample} Membros..."
+Member.create(email: 'member@gti.com', password: '123123', junior_enterprise_id: 1, validated: nil)
+ejs = Set.new(2..10)
 #5 membros que desejam ser diretores
-5.times do |i|
+4.times do |i|
   ej_id = ejs.to_a.sample
   ejs = ejs.delete(ej_id)
-  Member.create(name: "membro#{i+1}", email: "quero_ser_diretor_#{i}@gti.com", password: '123123', junior_enterprise_id: ej_id, validated: nil)
+  Member.create(name: "membro#{i+2}", email: "quero_ser_diretor_#{i}@gti.com", password: '123123', junior_enterprise_id: ej_id, validated: nil)
 end
 
 #5 diretores de EJ
@@ -36,3 +43,5 @@ end
   ejs = ejs.delete(ej_id)
   Member.create(name: "membro#{i+6}", email: "diretor_#{i}@gti.com", password: '123123', junior_enterprise_id: ej_id, validated: true)
 end
+
+puts "##### FIM #####"
