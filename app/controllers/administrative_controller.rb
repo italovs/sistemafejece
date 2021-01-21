fa69b5a657e77d8a6680e0e1236080a87c6cb0cc
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-01-21 12:23:47 -0300

    Melhorando resposta ajax

--- app/controllers/administrative_controller.rb
+++ app/controllers/administrative_controller.rb
@@ -1,25 +1,34 @@
 # frozen_string_literal: true
 
 class AdministrativeController < ApplicationController
-  layout 'administrative'
-  include ApplicationHelper
-  def index
-  end
+	layout 'administrative'
+	include ApplicationHelper
+	def index
+	end
 
-  def members_validation
-    @directors = Member.where(validated: true)
-    @not_yet_directors = Member.where(validated: nil)
-  end
+	def members_validation
+		return_members
+	end
 
-  def change_member_validation
-    membro = Member.find(params[:id])
-    membro.validated = params[:status]
-    ActiveRecord::Base.transaction do
-      membro.save
-      render json: [msg: "Sucesso"]
-    end
+	def change_member_validation
+		membro = Member.find(params[:id])
+		membro.validated = params[:status]
+		ActiveRecord::Base.transaction do
+			membro.save
+			@directors = Member.where(validated: true)
+			return_members
+			@directors = @directors.map { |m| [m.id, m.name, m.junior_enterprise.name] }
+			@not_yet_directors = @not_yet_directors.map { |m| [m.id, m.name, m.junior_enterprise.name] }
+			render json: [msg: "Sucesso", directors: @directors, members: @not_yet_directors]
+		end
 
-    rescue ActiveRecord::RecordInvalid
-      render json: [msg: "Erro"]
-  end
+		rescue ActiveRecord::RecordInvalid
+			render json: [msg: "Erro"]
+	end
+
+	private
+	def return_members
+		@directors = Member.where(validated: true)
+		@not_yet_directors = Member.where(validated: nil)
+	end
 end
