c208de2483166f4311d98e8656f2f295f511bc92
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-01-21 21:42:36 -0300

    Ajax funcionando

---
+++ app/assets/javascripts/members_validation.js
@@ -0,0 +1,29 @@
+$(function(){
+	$("div.btn.btn-primary").on("click", function(){
+		fire_ajax(this)
+	})
+})
+
+function fire_ajax( obj ){
+  id = $(obj).attr("id");
+  if( id.substring( 0, id.indexOf("_")) == "director" ){
+    director = true;
+  } else if( id.substring( 0, id.indexOf("_")) == "member" ){
+    director = false;
+  }
+  member_id = id.substring( id.indexOf("_") + 1, id.length)
+  $.post("/pirates/director/change_validation",
+  {
+    id: member_id,
+    status: director
+  },
+  function(data, status){
+    if(status == "success"){
+      refill_table("#membros", data[0]["members"], "member")
+      refill_table("#diretores", data[0]["directors"], "directors")
+    }
+    $("div.btn.btn-primary").on("click", function(){
+      fire_ajax(this)
+    })
+  });
+}
