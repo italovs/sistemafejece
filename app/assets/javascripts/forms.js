8a73f63c6aa4e50ec56997ea443f49c032a406b7
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-01-22 20:34:14 -0300

    Capturando dados para form ajax

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,14 +1,20 @@
-function verify_same_data( field_1, field_2){
-  if( ($(field_1).val() != "") && ($(($(field_1).val() != "")))    ($(field_1).val() != $(field_2).val())  ){
-    alert("Valores diferentes!")
+function verify_same_data( field_1, field_2, field_name){
+  if( ($(field_1).val() != "") && ($(($(field_1).val() != ""))) &&  ($(field_1).val() != $(field_2).val())  ){
+    return "Campos de "+field_name+" são diferentes!"
   }
 }
 
 function collect_data(){
   fields = {} //json
-  
-  
-  $(input).each(function(){
+  $(".input_field").each(function(){
     fields[ $(this).attr("name") ] = $(this).val();
   });
+  console.log(fields)
 }
+
+$(function(){
+  $("#send").on("click", function(){
+    collect_data();
+  });
+});
+
