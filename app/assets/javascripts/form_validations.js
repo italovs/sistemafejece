6bd19b1561b006d5b3700293b57cd858dea41db0
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-01-22 12:24:21 -0300

    Renomeando js

--- app/assets/javascripts/form_validations.js
+++
@@ -1,5 +0,0 @@
-function verify_same_data( field_1, field_2){
-  if( ($(field_1).val() != "") && ($(($(field_1).val() != "")))    ($(field_1).val() != $(field_2).val())  ){
-    alert("Valores diferentes!")
-  }
-}
