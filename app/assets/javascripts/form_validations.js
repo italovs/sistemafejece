54b6dc3461c5c4bb4c631acd47c254fb0020783e
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-01-21 23:12:37 -0300

    Alterações em form

---
+++ app/assets/javascripts/form_validations.js
@@ -0,0 +1,5 @@
+function verify_same_data( field_1, field_2){
+  if( ($(field_1).val() != "") && ($(($(field_1).val() != "")))    ($(field_1).val() != $(field_2).val())  ){
+    alert("Valores diferentes!")
+  }
+}
