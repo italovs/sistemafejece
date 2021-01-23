3266f4fb9a554169326b90bf4d4fd77678eef2ff
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-01-22 22:26:23 -0300

    Array de erros para form ok

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,20 +1,36 @@
-function verify_same_data( field_1, field_2, field_name){
-  if( ($(field_1).val() != "") && ($(($(field_1).val() != ""))) &&  ($(field_1).val() != $(field_2).val())  ){
-    return "Campos de "+field_name+" são diferentes!"
-  }
+function verify_same_data( data_1, data_2, field_name, errors_array){
+  if( (data_1 != data_2) && ((data_1 != "") && (data_2 != "")) ){
+    errors_array.push("Campos de "+field_name+" são diferentes!")
+  } 
+  return errors_array;
+  
 }
 
-function collect_data(){
-  fields = {} //json
+function collect_data( redundant_fields = [] ){
+  fields = {};
+  errors = []
   $(".input_field").each(function(){
-    fields[ $(this).attr("name") ] = $(this).val();
+    this_field = $(this).attr("name"); 
+    fields[ this_field ] = $(this).val();
+    if( fields[ this_field ] == "" ){
+      if( this_field.indexOf("_2") != -1 ){
+        invalid_field = "repetição de "+ this_field.substring(0, this_field.length - 2);
+      } else {
+        invalid_field = this_field
+      }
+      errors.push("Campo de "+invalid_field+" não pode estar em branco")
+    }
   });
-  console.log(fields)
+
+  for(i = 0; i < redundant_fields.length; i++){
+    errors = verify_same_data( fields[redundant_fields[i]], fields[redundant_fields[i] +"_2"], redundant_fields[i], errors )
+  }
+  console.log(errors)
 }
 
 $(function(){
   $("#send").on("click", function(){
-    collect_data();
+    collect_data([ "email", "senha" ]);
   });
 });
 
