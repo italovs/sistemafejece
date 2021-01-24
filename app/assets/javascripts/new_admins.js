6aae77a0137a865de84c5116e81904cc16fc09ac
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-01-24 17:00:18 -0300

    Correção de JS para diretores

--- app/assets/javascripts/new_admins.js
+++ app/assets/javascripts/new_admins.js
@@ -2,7 +2,7 @@ $(function(){
 	$("#send").on("click", function(){
 		response = collect_data([ "email", "senha" ]);
 		if( response[0] == true ){
-			ajax_submit(response[1], "/pirates/new_pirates")			
+			ajax_submit(response[1], "/pirates/new_pirates", true)
 		} else {
 			// alert(response[1])
 			//EXIBIR ERROS NA TELA
