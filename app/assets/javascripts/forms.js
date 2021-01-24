2a4f56bfc128433e17bc4bb635dcb53ec123e4bb
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-01-24 10:23:04 -0300

    Remanejando cógidos de JS de admins

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -46,16 +46,3 @@ function ajax_submit(fields, target_path){
     
   });
 }
-
-$(function(){
-	$("#send").on("click", function(){
-		response = collect_data([ "email", "senha" ]);
-		if( response[0] == true ){
-			ajax_submit(response[1], "/pirates/new_pirates")			
-		} else {
-			// alert(response[1])
-			//EXIBIR ERROS NA TELA
-		}
-	});
-});
-
---
+++ app/assets/javascripts/new_admins.js
@@ -0,0 +1,11 @@
+$(function(){
+	$("#send").on("click", function(){
+		response = collect_data([ "email", "senha" ]);
+		if( response[0] == true ){
+			ajax_submit(response[1], "/pirates/new_pirates")			
+		} else {
+			// alert(response[1])
+			//EXIBIR ERROS NA TELA
+		}
+	});
+});
