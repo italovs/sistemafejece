4eed5a45db0915f1c7b214ed69b899c0559be55a
italoviana33 <italo_1002@live.com>
2021-02-06 14:17:28 -0300

    interface view de cadastro

--- app/controllers/Members/registrations_controller.rb
+++ app/controllers/Members/registrations_controller.rb
@@ -4,10 +4,11 @@ class Members::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+   GET /resource/sign_up
+   def new
+    junior_enterprises_options_function
+    super
+   end
 
   # POST /resource
   # def create
@@ -38,12 +39,12 @@ class Members::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
-
+  protected
+   
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :position, :junior_enterprises_id])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
