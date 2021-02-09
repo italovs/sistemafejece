7f87fa206986618cd284994a636a912495b8a68b
gabrie1 <joaogabriel9086@gmail.com>
2021-02-09 15:38:41 -0300

    Tabelas de Diretor, EJs e Categorias

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module Example
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.i18n.default_locale = :'pt-BR'
+    config.i18n.default_locale = :"pt-BR"
   end
 end
