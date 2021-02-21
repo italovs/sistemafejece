995c0a11270f0a02b0cc060bfd79b456a97980cd
italoviana33 <italo_1002@live.com>
2021-02-21 16:47:11 -0300

    troca do paperclip por activestorage e correçao em views

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,7 +3,7 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
-  has_attached_file :picture, styles: { medium: "300x300>", small: "30x30#" }, default_url: "assets/:style/user.png"
-  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+  # has_attached_file :picture, styles: { medium: "300x300>", small: "30x30#" }, default_url: "assets/:style/user.png"
+  #validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   
 end
