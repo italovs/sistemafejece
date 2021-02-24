e11bc158ca328f64cb73b65e644528c74c43ceab
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-02-24 07:41:21 -0300

    helpers e ação em controller

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,18 @@
 module SiteHelper
+
+	def video_channel_routes
+		if member_signed_in?
+			validated_tv_channel_path
+		else
+			admin_tv_channel_path
+		end
+	end
+
+	def valid_member_or_admin?
+		if admin_signed_in?
+			true
+		else
+			current_member.valid?
+		end
+	end
 end
