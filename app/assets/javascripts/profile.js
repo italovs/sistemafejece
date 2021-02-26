98cd916735262b135958ac0aa97359678d7a87e3
gabrie1 <joaogabriel9086@gmail.com>
2021-02-26 15:13:47 -0300

    Ajustes no layout

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,24 +1,5 @@
 //= require jquery
 
-//Navbar
-var menu_toggler = $("#mobile-nav-toggler");
-var menu_API = main_menu.data( "mmenu" );
-
-menu_toggler.on( "click", function() {
-	menu_API.open();
-	document.getElementById("mobile-nav-toggler").style.margin = "0px 450px 0px 0px";
-	$(".logo-white").hide();
-});
-
-var menuClose = $(".mm-slideout");
-
-menuClose.on("click", function(){
-	document.getElementById("mobile-nav-toggler").style.margin = "0px 0px 0px 0px";
-	$(".logo-white").show();
-});
-//End of Navbar
-
-
 $(function(){
 	$("#send").on("click", function(){
 		response = collect_data([ "email", "senha" ]);
@@ -205,3 +186,22 @@ function update_data( new_data ){
 		$("#validated").html("Diretoria Solicitada")
 	}
 }
+
+
+//Navbar
+var menu_toggler = $("#mobile-nav-toggler");
+var menu_API = main_menu.data( "mmenu" );
+
+menu_toggler.on( "click", function() {
+	menu_API.open();
+	document.getElementById("mobile-nav-toggler").style.margin = "0px 450px 0px 0px";
+	$(".logo-white").hide();
+});
+
+var menuClose = $(".mm-slideout");
+
+menuClose.on("click", function(){
+	document.getElementById("mobile-nav-toggler").style.margin = "0px 0px 0px 0px";
+	$(".logo-white").show();
+});
+//End of Navbar
