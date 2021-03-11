59b00d5edb34cc5ed5387b36143380a5082c56a3
gabrie1 <joaogabriel9086@gmail.com>
2021-03-10 22:47:02 -0300

    Tentativa de fazer paginaçao

--- app/assets/javascripts/my_channel.js
+++ app/assets/javascripts/my_channel.js
@@ -21,32 +21,63 @@ function page_load(){
 	}
 	starting_from_videos();
 	initial_create_table();
-	sucesso();
 }
 
 function page_reload(){
 	hide_fields();
 	starting_from_videos();
 	initial_create_table();
-	sucesso();
 }
 
-function sucesso(){
-    $('#demo').pagination({
-        dataSource: [1, 2, 3, 4, 5],
-        pageSize: 2,
-        showPrevious: false,
-        showNext: false,
-        callback: function(data, pagination) {
-            // template method of yourself
-            var html = template(data);
-            dataContainer.html(html);
-        }
-    })
+		var table = "#my_table";
+        $("#maxRows").on('change', function(){
+        	$(".pagination").html()
+			var trnum = 0;
+        	var maxRows = parseInt($(this).val());
+        	var totalRows = $(table+'tbody tr').lenght
 
+			$(table+' tr:gt(0)').each(function(){
+				trnum++;
+				if(trnum > maxRows){
+					$(this).hide();
+				}
+				if (trnum <= maxRows){
+					$(this).show();
+				}
+			})
 
-}
+          if(totalRows > maxRows){
+            var pagenum = Math.ceil(totalRows/maxRows)
+            for(var i=1; i<=pagenum; ){
+              $('.pagination').append('<li data-page="'+i+'">\<span>' + i++ +'<span class="sr-only">(current)</span></span>\</li>').show();
+            }
+          }
+          $('.pagination li:first-child').addClass('active')
+          $('.pagination li').on('click', function(){
+            var pageNum = $(this).attr('data-page');
+            var trIndex = 0;
+            $('.pagination li').removeClass('active')
+            $(this).addClass('active')
+            $(table+' tr:gt(0)').each(function(){
+              trIndex++;
+              if(trIndex > (maxRows*pageNum) || trIndex <= ((maxRows*pageNum)-maxRows)){
+                $(this).hide();
+              }else{
+                $(this).show();
+              }
+            })
+          })
 
+          $(function(){
+            $('table tr:eq(0)').prepend('#')
+            var id = 0;
+            $('table tr:gt(0)').each(function(){
+              id++
+              $(this).prepend('<td>'+id+'</td>')
+            })
+          })
+        }) 
+		
 function starting_from_videos(){
 	$(".videos-row").show();
 	$(".series-row").hide();
