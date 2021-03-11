2ab54627b9061eb39ca6c48ff1a4dc37fdc86e7b
italoviana33 <italo_1002@live.com>
2021-03-11 15:01:56 -0300

    pagination minhas series

--- app/assets/javascripts/my_channel.js
+++ app/assets/javascripts/my_channel.js
@@ -21,6 +21,7 @@ function page_load(){
 	}
 	starting_from_videos();
 	initial_create_table();
+	pagination();
 }
 
 function page_reload(){
@@ -29,55 +30,57 @@ function page_reload(){
 	initial_create_table();
 }
 
-		var table = "#my_table";
-        $("#maxRows").on('change', function(){
-        	$(".pagination").html()
-			var trnum = 0;
-        	var maxRows = parseInt($(this).val());
-        	var totalRows = $(table+'tbody tr').lenght
+var table = "#mytable";
+$("#maxRows").on('change', pagination)
 
+function pagination(){
+	$('.pagination').html('')
+		var trnum = 0;
+		var maxRows = parseInt($('#maxRows').val());
+		var totalRows = $(table+'  tbody tr').length
+
+		$(table+' tr:gt(0)').each(function(){
+			trnum++;
+			if(trnum > maxRows){
+				$(this).hide();
+			}
+			if (trnum <= maxRows){
+				$(this).show();
+			}
+		})
+
+		if(totalRows > maxRows){
+			var pagenum = Math.ceil(totalRows/maxRows)
+			for(var i=1; i<=pagenum; ){
+				$('.pagination').append('<li data-page="'+i+'" class="page-item">\<a class="page-link">' + i++ +'<a class="sr-only">(current)</a></a>\</li>').show();
+			}
+		}
+		$('.pagination li:first-child').addClass('active')
+		$('.pagination li').on('click', function(){
+			var pageNum = $(this).attr('data-page');
+			var trIndex = 0;
+			$('.pagination li').removeClass('active')
+			$(this).addClass('active')
 			$(table+' tr:gt(0)').each(function(){
-				trnum++;
-				if(trnum > maxRows){
+				trIndex++;
+				if(trIndex > (maxRows*pageNum) || trIndex <= ((maxRows*pageNum)-maxRows)){
 					$(this).hide();
-				}
-				if (trnum <= maxRows){
+				}else{
 					$(this).show();
 				}
 			})
+		})
 
-          if(totalRows > maxRows){
-            var pagenum = Math.ceil(totalRows/maxRows)
-            for(var i=1; i<=pagenum; ){
-              $('.pagination').append('<li data-page="'+i+'">\<span>' + i++ +'<span class="sr-only">(current)</span></span>\</li>').show();
-            }
-          }
-          $('.pagination li:first-child').addClass('active')
-          $('.pagination li').on('click', function(){
-            var pageNum = $(this).attr('data-page');
-            var trIndex = 0;
-            $('.pagination li').removeClass('active')
-            $(this).addClass('active')
-            $(table+' tr:gt(0)').each(function(){
-              trIndex++;
-              if(trIndex > (maxRows*pageNum) || trIndex <= ((maxRows*pageNum)-maxRows)){
-                $(this).hide();
-              }else{
-                $(this).show();
-              }
-            })
-          })
-
-          $(function(){
-            $('table tr:eq(0)').prepend('#')
-            var id = 0;
-            $('table tr:gt(0)').each(function(){
-              id++
-              $(this).prepend('<td>'+id+'</td>')
-            })
-          })
-        }) 
-		
+	
+}
+$(function(){
+	$('table tr:eq(0)').prepend('<th>ID</th>')
+	var id = 0;
+	$('table tr:gt(0)').each(function(){
+		id++
+		$(this).prepend('<td>'+id+'</td>')
+	})
+}) 
 function starting_from_videos(){
 	$(".videos-row").show();
 	$(".series-row").hide();
