3d09e91e31dae568f10b43de8e6047fc0fce9a81
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-03-31 00:40:11 -0300

    Migração para coluna de views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_235357) do
+ActiveRecord::Schema.define(version: 2021_03_31_032753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 2021_03_16_235357) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "kind"
     t.integer "owner_id"
+    t.integer "views"
   end
 
   create_table "season_posts", force: :cascade do |t|
