ac3c9f60d4f88364145e08c3a6966c2aa499d707
italoviana33 <italo_1002@live.com>
2021-04-10 20:30:27 -0300

    configuraçao de cargos e solicitaçao de diretoria no cadastro

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,22 +1,21 @@
 class Member < ApplicationRecord
-	# Include default devise modules. Others available are:
-	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-	attribute :name, :string, default: ""
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  attribute :name, :string, default: ''
 
-	belongs_to :junior_enterprise
-	has_one_attached :profile_picture do |attachable|
-		attachable.variant(combine_options:{gravity:'Center', crop: '100x100+0+0'})
-		attachable.variant(combine_options:{gravity:'Center', crop: '300x300+0+0'})
-	end
-	validates :profile_picture, content_type: ["image/jpg","image/png","image/jpeg"]
-	validates :profile_picture, size: {less_than: 5.megabytes}
-	
-	enum position: {
-		desenvolvedor:0,
-		designer: 1,
-		marketing: 2,
-		gerente: 3,
-		rh: 4
-	}
+  belongs_to :junior_enterprise
+  has_one_attached :profile_picture do |attachable|
+    attachable.variant(combine_options: {gravity: 'Center', crop: '100x100+0+0'})
+    attachable.variant(combine_options: {gravity: 'Center', crop: '300x300+0+0'})
+  end
+  validates :profile_picture, content_type: ['image/jpg', 'image/png', 'image/jpeg']
+  validates :profile_picture, size: {less_than: 5.megabytes}
+
+  enum position: {
+    Consultor: 0,
+    Gerente: 1,
+    Coordenador: 2,
+    Diretor: 3
+  }
 end
