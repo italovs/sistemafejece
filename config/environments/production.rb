b8e8983d91d820fcb15da8a4e40848681e22ee8b
italoviana33 <italo_1002@live.com>
2021-04-17 18:44:26 -0300

    yui compressor config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,8 +30,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.css_compressor = :yui
-  config.assets.js_compressor = :yui
+  config.assets.css_compressor = YUI::CssCompressor.new
+  config.assets.js_compressor = YUI::JavaScriptCompressor.new
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
