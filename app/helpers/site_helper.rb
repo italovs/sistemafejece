681071ba53ee029ea275190088481bf30dbbe8e1
italoviana33 <italo_1002@live.com>
2021-05-24 10:57:40 -0300

    fix devise notifications

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,26 +1,25 @@
 module SiteHelper
+  def my_channel_routes
+    if member_signed_in?
+      validated_my_channel_path
+    else
+      admin_my_channel_path
+    end
+  end
 
-	def my_channel_routes
-		if member_signed_in?
-			validated_my_channel_path
-		else
-			admin_my_channel_path
-		end
-	end
+  def my_library_routes
+    if member_signed_in?
+      validated_my_library_path
+    else
+      admin_my_library_path
+    end
+  end
 
-	def my_library_routes
-		if member_signed_in?
-			validated_my_library_path
-		else
-			admin_my_library_path
-		end
-	end
-
-	def valid_member_or_admin?
-		if admin_signed_in?
-			true
-		elsif current_member.validated?
-			true
-		end
-	end
+  def valid_member_or_admin?
+    if admin_signed_in?
+      true
+    elsif current_member.validated?
+      true
+    end
+  end
 end
