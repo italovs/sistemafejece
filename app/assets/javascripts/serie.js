a02a11e7beabd9f6b8213fb00425b8d3cd294b24
italoviana33 <italo_1002@live.com>
2021-06-16 09:49:43 -0300

    series page add content type to cards

--- app/assets/javascripts/serie.js
+++ app/assets/javascripts/serie.js
@@ -35,16 +35,18 @@ function starting_from_videos(){
 
 function post_from_list(post, poster_image, rating, i){
     $("#posts-list").append("<div class='col-md-12 col-sm-12 var-item p-0'><div class='movie-list-1 mb30'><div class='listing-container'><div class='listing-image'><img src='"+poster_image+
-    "'><div class='play-btn'></div></div><div class='stars'></div><div class='listing-content'><div class='inner'><h2 class='title'>"+post["name"]+
+    "'><div class='play-btn'></div></div><div class='stars'></div><div class='listing-content'><div class='inner' id='infos_"+post["id"]+"'><h2 class='title'>"+post["name"]+
     "</h2><p>"+post["description"]+"</p><a class='btn btn-main btn-effect' href='/post/"+post["id"]+"'>Detalhes</a></div></div></div></div></div>")
 
     if (post["kind"] == "video")
     {
         $(".play-btn").eq(i).append("<a href='https://www.youtube.com/watch?v="+post["link"]+"' class='play-video' id='"+post["id"]+"'><i class='fas fa-play'></i></a>")
+        $("#infos_"+post["id"]).prepend("<span class='content_type'>Vídeo</span>")
     }
     else if (post["kind"] == "post")
     {
         $(".play-btn").eq(i).append("<a href='"+post["link"]+"' class='open-post' id='"+post["id"]+"'><i class='fas fa-link'></i></a>")
+        $("#infos_"+post["id"]).prepend("<span class='content_type'>Material Complementar</span>")
     }
 
     if (rating == -1)
