9bbeb8e62346e351e0e362d76f29aa444b252f2a
italoviana33 <italo_1002@live.com>
2021-06-22 23:07:14 -0300

    force ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+   config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -64,10 +64,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "example_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {host: 'https://farol-fejece.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'https://farol.fejece.com.br'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    domain: 'farol-fejece.herokuapp.com',
+    domain: 'farol.fejece.com.br',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
